e3bbb954e2e88dec8a68e921021f8befd3d25156
rinti <andreas@agoodid.se>
2016-08-03 18:24:00 +0200

    use booli api instead

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const _ = require("lodash")
-const cheerio = require("cheerio")
 const fetch = require("node-fetch")
 const RSVP = require("rsvp")
+const Booli = require("booli-api")
 
-// config
-const GOOGLE_KEY = "" // need a valid google distance matrix key
-const DESTINATION = "Stockholm Centralstation" // where you want to go
-const FILTER_URL = "https://www.booli.se/solna,stockholms+innerstad,gamla+enskede,bromma,johanneshov,grondal,enskede,enskede+gard,enskededalen,hammarbyhojden,liljeholmen/35,143,1454,115355,874645,874646,874647,874648,874651,874652,874654/?direction=asc&isNewConstruction=0&maxListPrice=2400000&maxRent=3000&minLivingArea=25&sort=listSqmPrice" // any filtered booli url you want, this was what I was searching for.
+GOOGLE_KEY = ""
+BOOLI_API_KEY = ""
+BOOLI_CALLER_ID = ""
+DESTINATION = ""
 
-
-let origins = []
-
-const promise = fetch(FILTER_URL).then(resp => resp.text()).then((resp) => {
-  let $ = cheerio.load(resp)
-  $(".search-list__row.search-list__row--bold.search-list__row--address").each((i, item) => {
-    origins.push($(item).text())
-  });
-});
+booli = new Booli(BOOLI_CALLER_ID, BOOLI_API_KEY)
 
 
 const textToMinutes = (text) => {
@@ -26,30 +18,38 @@ const textToMinutes = (text) => {
   return (parseInt(hours, 10) * 60) + parseInt(mins, 10)
 }
 
-const getDistanceAndTime = (origin) => {
-  origin = origin + ', Stockholm' // failsafe if the address is available in another city
+const getDistanceAndTime = (lat, lng) => {
   var url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&mode=transit&origins="
-    + encodeURIComponent(origin)
+    + encodeURIComponent(lat)
+    + ","
+    + encodeURIComponent(lng)
     + "&destinations="
     + encodeURIComponent(DESTINATION)
     + "&key=" + GOOGLE_KEY
-
-  return fetch(url).then(resp => resp.json())
+  return fetch(url).then((resp) => { return resp.json() })
 };
 
+const search_params = {
+  areaId: "35,143,1454,7300,115355,874645,874646,874647,874648,874651,874652,874654",
+  isNewConstruction: 0,
+  maxListPrice: 2400000,
+  maxRent: 3000,
+  minLivingArea: 25,
+  limit: 500
+}
 
-promise.then(() => {
-  origins = origins.map(dest => getDistanceAndTime(dest))
-  RSVP.all(origins).then((items) => {
-    items = items
-      .map((item) => {
-        return {
-          from: item.origin_addresses[0],
-          verbose: item.rows[0].elements[0].duration.text,
-          time: textToMinutes(item.rows[0].elements[0].duration.text)
-        }
-      })
-    items = _.sortBy(items, "time")
-    console.log(items.reverse())
+booli.listings(search_params).then(res => res.json()).then((res) => {
+  res.listings.forEach((item) => {
+    let promise = getDistanceAndTime(item.location.position.latitude, item.location.position.longitude);
+    promise.then((resp) => {
+      if(item.listPrice < 2000000 && textToMinutes(resp.rows[0].elements[0].duration.text) < 30) {
+        console.log(item.location.namedAreas[0])
+        console.log(resp.rows[0].elements[0].duration.text)
+        console.log(item.listPrice)
+        console.log(item.url)
+        console.log("------------------")
+      }
+    })
   })
 })
+
