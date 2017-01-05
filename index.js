608f2b3c7407f87efa0113b74cd0aaa70f51899d
rinti <andreas@agoodid.se>
2017-01-05 14:52:30 +0100

    use async/await

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const _ = require("lodash")
 const fetch = require("node-fetch")
-const RSVP = require("rsvp")
 const Booli = require("booli-api")
 
-GOOGLE_KEY = ""
-BOOLI_API_KEY = ""
-BOOLI_CALLER_ID = ""
-DESTINATION = ""
+const async = require('asyncawait/async')
+const await = require('asyncawait/await')
+
+const GOOGLE_KEY = process.env.GOOGLE_API_KEY
+const BOOLI_API_KEY = process.env.BOOLI_API_KEY
+const BOOLI_CALLER_ID = process.env.BOOLI_CALLER_ID
+const DESTINATION = process.env.DESTINATION
 
 booli = new Booli(BOOLI_CALLER_ID, BOOLI_API_KEY)
-
 
 const textToMinutes = (text) => {
   let hours = text.match(/(\d+) hour/)
@@ -18,7 +19,7 @@ const textToMinutes = (text) => {
   return (parseInt(hours, 10) * 60) + parseInt(mins, 10)
 }
 
-const getDistanceAndTime = (lat, lng) => {
+var getDistanceAndTime = async(function(lat, lng) {
   var url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&mode=transit&origins="
     + encodeURIComponent(lat)
     + ","
@@ -26,8 +27,13 @@ const getDistanceAndTime = (lat, lng) => {
     + "&destinations="
     + encodeURIComponent(DESTINATION)
     + "&key=" + GOOGLE_KEY
-  return fetch(url).then((resp) => { return resp.json() })
-};
+  try {
+    let response = await(fetch(url))
+    return await(response.json())
+  } catch(err) {
+    console.log("ERROR: ", err)
+  }
+})
 
 const search_params = {
   areaId: "35,143,1454,7300,115355,874645,874646,874647,874648,874651,874652,874654",
@@ -35,21 +41,26 @@ const search_params = {
   maxListPrice: 2400000,
   maxRent: 3000,
   minLivingArea: 25,
-  limit: 500
+  limit: 3
 }
 
-booli.listings(search_params).then(res => res.json()).then((res) => {
-  res.listings.forEach((item) => {
-    let promise = getDistanceAndTime(item.location.position.latitude, item.location.position.longitude);
-    promise.then((resp) => {
-      if(item.listPrice < 2000000 && textToMinutes(resp.rows[0].elements[0].duration.text) < 30) {
-        console.log(item.location.namedAreas[0])
-        console.log(resp.rows[0].elements[0].duration.text)
-        console.log(item.listPrice)
-        console.log(item.url)
-        console.log("------------------")
-      }
-    })
-  })
+var getDataz = async(function() {
+  let listings = await(booli.listings(search_params))
+  let res = await(listings.json())
+  let counter = 0
+  let items = await(res.listings.map(async((item) => {
+    let dist = await(getDistanceAndTime(item.location.position.latitude, item.location.position.longitude))
+    let d = dist.rows[0].elements[0]
+    return {
+      meters: d.distance.value,
+      seconds: d.duration.value,
+      item
+    }
+  })))
+  return items
 })
 
+getDataz().then((items) => {
+  items = _.orderBy(items, (i) => i.duration.value, 'desc')
+  console.log(items)
+})
